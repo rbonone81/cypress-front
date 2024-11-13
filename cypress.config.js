67d6c5652d8b434a7ae5bec3dd44748049ee5bc0
Rodrigo Bonone Moreira <rbonone@gmail.com>
2024-11-13 17:57:07 -0300

    Add reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
+  },
   e2e: {
     viewportWidth: 1600,
     viewportHeight: 800,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
